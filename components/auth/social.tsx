ca0094c68176a95130033f9d46130660ff3636ff
aaronwai <aaron.lung.wai@gmail.com>
2025-11-01 20:15:44 +0800

    Step 3 : login-button with card

---
+++ components/auth/social.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+export const Social = () => {
+  return (
+    <div className='flex items-center w-full gap-x-2'>
+      <Button
+        variant='outline'
+        size='lg'
+        className='flex-1 aspect-square px-0'
+        onClick={() => {}}
+      >
+        <FcGoogle className='h-5 w-5' />
+      </Button>
+      <Button
+        variant='outline'
+        size='lg'
+        className='flex-1 aspect-square px-0'
+        onClick={() => {}}
+      >
+        <FaGithub className='h-5 w-5' />
+      </Button>
+    </div>
+  );
+};
